2e0517f860e6957e390be4ccd1f9a65fa5ab1f3f
hun-jae <61400572+hun-jae@users.noreply.github.com>
2023-03-10 11:27:19 +0900

    [Silver III] Title: 로마 숫자 만들기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/16922. 로마 숫자 만들기/로마 숫자 만들기.py
+++ 백준/Silver/16922. 로마 숫자 만들기/로마 숫자 만들기.py
@@ -1,11 +1,12 @@
-n=int(input())
+N=int(input())
 nums=[]
 numList = [1,5,10,50]
 def search(depth, num, start):
-    if depth==n:
+    if depth==N:
         nums.append(num)
         return
     for i in range(start, 4):
         search(depth+1, num+numList[i], i)
 search(0,0,0)
 print(len(set(nums)))
+#edit test
