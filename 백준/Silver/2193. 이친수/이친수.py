1e94b58ee8ac2fafe9139cca9841751fe8797897
hun-jae <61400572+hun-jae@users.noreply.github.com>
2023-08-23 16:17:57 +0900

    [Silver III] Title: 이친수, Time: 68 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.py
@@ -0,0 +1,8 @@
+n = int(input())
+dp = [[0 for _ in range(n+1)] for _ in range(2)]
+dp[0][1] = 0
+dp[1][1] = 1
+for i in range(2, n+1, 1):
+    dp[0][i] = dp[0][i-1] + dp[1][i-1]
+    dp[1][i] = dp[0][i-1]
+print(dp[0][n] + dp[1][n])
