3d9f9a86b491c3b02a99b8cad7077e9b9f15cc2b
hun-jae <61400572+hun-jae@users.noreply.github.com>
2024-03-10 19:16:30 +0900

    [Silver III] Title: 터렛, Time: 56 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Silver/1002. 터렛/터렛.py
+++ 백준/Silver/1002. 터렛/터렛.py
@@ -5,17 +5,11 @@ tc = int(input())
 for _ in range(tc):
     x1, y1, ac, x2, y2, bc = map(int, input().split())
     ab = math.sqrt(abs(x1 - x2)**2 + abs(y1 - y2)**2)
-    if x1 == x2 and y1 == y2:
-        if ac != bc:
-            print(0)
-        else:
-            print(-1)
+    if x1 == x2 and y1 == y2 and ac == bc:
+        print(-1)
+    elif abs(ac - bc) < ab < ac + bc:
+        print(2)
+    elif abs(ac - bc) == ab or ac + bc == ab:
+        print(1)
     else:
-        if abs(ac - bc) < ab < ac + bc:
-            print(2)
-        elif abs(ac - bc) == ab or ac + bc == ab:
-            print(1)
-        elif ab > ac + bc:
-            print(0)
-        else:
-            print(0)
+        print(0)
