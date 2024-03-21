9dbcda30991312b86be30fe75301e50dcf4fefa3
hun-jae <61400572+hun-jae@users.noreply.github.com>
2024-03-21 16:06:32 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 0.05 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.py
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.py
@@ -1,30 +1,28 @@
 def next(users, emoticons, arr_dis, max_num_join, max_price):
-    if len(arr_dis) == len(emoticons):  # 디스카운트 설정 끝
-        # 여기서 계산
+    if len(arr_dis) == len(emoticons):
         num_join = 0
         price = 0
+        
         for percent, join_price in users:
             tmp_price = 0
+            
             for i, dis in enumerate(arr_dis):
                 if dis >= percent:
                     tmp_price += emoticons[i]*(100-dis)//100
-            if tmp_price >= join_price: #해당 유저가 가입함
+                    
+            if tmp_price >= join_price: #가입 o
                 num_join += 1
-            else: #해당 유저가 가입하지 않음
+            else: #가입 x
                 price += tmp_price
 
         if num_join > max_num_join:
-            # print(num_join)
-            # print(price)
-            # print(arr_dis)
-
             max_num_join = num_join
             max_price = price
 
         elif num_join == max_num_join:
             max_price = max(max_price, price)
 
-    else:  # 아직 이모티콘 할인율이 전부 정해지지 않음
+    else:
         for dis in [10, 20, 30, 40]:
             arr_dis.append(dis)
             max_num_join, max_price = next(users, emoticons, arr_dis, max_num_join, max_price)
